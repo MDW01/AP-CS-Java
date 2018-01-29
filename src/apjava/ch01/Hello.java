210e60ca8a458ffab8eadeffbfeb9070574b07de
MDW01 <michaeldwagner@me.com>
2018-01-29 12:47:45 -0600

    ch01

---
+++ src/apjava/ch01/Hello.java
@@ -0,0 +1,12 @@
+package apjava.ch01;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        // generate some simple output
+        System.out.println("Hello, World!");
+        System.out.println("How are you?");
+        System.out.print("Goodbye, ");
+        System.out.println("cruel world");
+    }
+}
---
+++ src/apjava/ch01/TextMonster.java
@@ -0,0 +1,10 @@
+package apjava.ch01;
+
+public class TextMonster {
+    public static void main(String[] args){
+        System.out.println("  \\ ___|___ /  ");
+        System.out.println("  _|  o o  |_   ");
+        System.out.println("   |___-___|    ");
+        System.out.println("  /    |    \\  ");
+    }
+}
